8a7f97e4e206a80131982a639da52fd40133b845
18251843020 <1038184898@qq.com>
2016-12-20 20:44:56 +0800

    添加项目

--- public/js/index.js
+++ public/js/index.js
@@ -1,64 +1,16 @@
 /**
- * Created by Administrator on 2016/12/7 0007.
+ * Created by Administrator on 2016/12/19 0019.
  */
 
-var startX,
-	startY,
-	moveX,
-	moveY,
-	times= 0,
-	timeOut;
-
-var dom =document.getElementsByClassName('md')[0]
-	dom.addEventListener('touchstart',function(e){
-		console.log('touchstart')
-		times=0;
-		timeOut = setInterval(function(){
-			times += 100;
-		},100)
-	var th = e.touches[0];
-		startX = th.clientX;
-		startY = th.clientY;
-})
-
-
-dom.addEventListener('touchmove',function(e){
-	var th = e.touches[0];
-	moveX = th.clientX;
-	moveY = th.clientY;
-})
-
-
-dom.addEventListener('touchend',function(e){
-	console.log('touchend')
-	console.log(startX,startY,moveX,moveY)
-
-	if (moveY -startY>=30){
-		dom.style.color='blue'
-	}else if(startY -moveY>=30){
-		dom.style.color='white'
-	}
-
-	var fontSize = parseInt(window.getComputedStyle(dom).fontSize);
-
-	console.log(fontSize)
-
-	if(moveX - startX >= 30){
-		fontSize += 2;
-		dom.style.fontSize = fontSize +'px'
-	}else if(startX - moveX>=30){
-		if(fontSize>=14){fontSize -= 2;
-			dom.style.fontSize = fontSize +'px'}
-
-	}
-	clearInterval(timeOut);
-	console.log(times);
-	if(times >=1000){
-		dom.style.color='red'
-	}
+$('.index-icon').on('tap', function(){
+	location.href = '../index/index.html'
 });
-
-
-var xhr = new XMLHttpRequest();
-
-
+$('.order-icon').on('tap', function(){
+	location.href = '../order/order.html'
+});
+$('.news-icon').on('tap', function(){
+	location.href = '../msg/msg.html'
+});
+$('.info-icon').on('tap', function(){
+	location.href = '../info/info.html'
+});
